685ece3c883a4d928d23580268fe328e16a65b97
Amine-Triki <amine.triki15@gmail.com>
2024-10-03 20:57:52 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat_mobile_app/screens/chat_screen.dart';
+
+import 'screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/chat_screen.dart';
+import 'screens/signin_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
